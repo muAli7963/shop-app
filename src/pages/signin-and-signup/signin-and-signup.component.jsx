15fac31fb467b31eaee22335965335bd82161856
mu ali <elshrab90.ma@gamil.com>
2019-10-31 14:56:29 +0200

    finish signin component

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignIn from '../../components/signin/signin.component'
+import './signin-and-signup.styles.scss';
+
+const SignInAndSignUp = () => {
+  return (
+    <div>
+        <SignIn />
+    </div>
+  )
+}
+
+export default SignInAndSignUp;
