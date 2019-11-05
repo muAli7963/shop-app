8d0be0806a4449825f5a00ea7159815237b9c94e
mu ali <elshrab90.ma@gamil.com>
2019-11-05 02:01:24 +0200

    finish signin and signup

--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/signin/signin.component'
+import SignUp from '../../components/signup/signup.component';
 import './signin-and-signup.styles.scss';
 
 const SignInAndSignUp = () => {
   return (
-    <div>
+    <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
   )
 }
